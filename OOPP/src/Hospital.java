da2c370ac6d4dfb57189edd3ace2ef90a5e22620
Melkamu <melkemech@gmail.com>
2025-09-25 10:10:39 +0300

    1. Appointment method 2. Service time method 3. Get name method 4. Get service time method 5. Capacity method 6. Serve patient method added and edited to the Hospital class

--- OOPP/src/Hospital.java
+++ OOPP/src/Hospital.java
@@ -1,18 +1,34 @@
 public abstract class Hospital {
     private String name;
-    private double time;
+    private int serviceTime;
+    private int capacity;
 
-    public Hospital(String name, double time) {
+    public Hospital(String name, int serviceTime, int capacity) {
         this.name = name;
-        this.time = time;
+        this.serviceTime = serviceTime;
+        this.capacity = capacity;
     }
+    public abstract void appointment(String patientName);
 
     public String getName() {
         return name;
     }
 
-    public double getTime(){
-        return time;
+    public double getServiceTime(){
+        return serviceTime;
+    }
+
+    public int getCapacity(){
+        return capacity;
+    }
+
+    public void setServiceTime(int serviceTime){
+        this.serviceTime = serviceTime;
+    }
+
+    public void servePatient(String patientName){
+        System.out.println("Serving patient " + patientName + " at " + name + " for " + serviceTime + " minutes.");
+
     }
 
 }
